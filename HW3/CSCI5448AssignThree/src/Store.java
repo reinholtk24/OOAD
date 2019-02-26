16859edaeae1c1c28a6777747214c1b9ee81a39b
Gabriella <g.johnson4520@gmail.com>
2019-02-26 12:25:04 -0700

    Create Store.java

---
+++ HW3/CSCI5448AssignThree/src/Store.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+	private List<Customer> customers;
+	private Inventory inventory;
+	private Report report;
+	private boolean isOpen;
+	
+	public Store() {
+		customers = new ArrayList<Customer>();
+		Customer c1 = new Casual("Monica");
+		Customer c2 = new Casual("Chandler");
+		Customer c3 = new Casual("Phoebe");
+		Customer b1 = new Business("Joey");
+		Customer b2 = new Business("Ross");
+		Customer b3 = new Business("Rachel");
+		Customer r1 = new Regular("Paige");
+		Customer r2 = new Regular("Piper");
+		Customer r3 = new Regular("Chris");
+		Customer r4 = new Regular("Wyatt");
+		
+		customers.add(c1);
+		customers.add(c2);
+		customers.add(c3);
+		customers.add(b1);
+		customers.add(b2);
+		customers.add(b3);
+		customers.add(r1);
+		customers.add(r2);
+		customers.add(r3);
+		customers.add(r4);
+				
+	}
+	
+	public void assistCustomer(Customer customer) {
+		
+	}
+	
+	public void checkRentals(int day) {
+		
+	}
+	
+	public void checkInventory() {
+		
+	}
+	
+	public List<Customer> generateEligibleCustomers(int itemsAvailable) {
+		return customers; 
+	}
+}
